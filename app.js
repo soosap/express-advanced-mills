fdbd8162e02b7a5a38cd005be4de1156400eb131
soosap <prasath.soosaithasan@gmail.com>
2016-04-21 11:04:12 +0200

    Dockerize app environment

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ let app = express();
 
 let port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
+bookRouter.route('/books')
+    .get((req, res) => {
+        let responseJson = {hello: "This is my cool API"}
+
+        res.json(responseJson);
+    });
+
+app.use('/api', bookRouter);
+
 app.get('/', (req, res) => {
     res.send('welcome to my API');
 });
